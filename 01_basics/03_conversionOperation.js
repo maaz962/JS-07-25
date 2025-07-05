659db5fcefaa8932ab65ebf40c761c300b86f151
maaz962 <muhammadmaaz4405@gmail.com>
2025-07-05 16:29:57 +0000

    ConversionOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+//let score = 33;
+//let score = "33"
+//let score = "33abc"
+//let score = null
+//let score = undefined
+//let score = true
+//let score = "maaz"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+// let valueIntoNumber = Number(score)
+// console.log(typeof valueIntoNumber)
+// console.log(valueIntoNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+//let isLoggedIn = 1
+//let isLoggedIn = ""
+// let isLoggedIn = "maaz"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "maaz" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
