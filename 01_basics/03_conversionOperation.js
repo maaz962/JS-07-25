09f4a80de497f281d200af5fe1d8ddb428f3d988
maaz962 <muhammadmaaz4405@gmail.com>
2025-07-06 07:44:24 +0000

    conversion and comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -31,8 +31,69 @@
 // "maaz" => true
 
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//   ***** Operations *****
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+
+// let str1 = "Hi"
+// let str2 = " Maaz"
+ 
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((1 + 2) *3 / 4);
+
+// 1
+// console.log(+true)
+
+// //0
+// console.log(+ "")
+
+// //error
+// console.log(true+);
+
+
+// let gameCounter = 100
+// gameCounter++
+// console.log(gameCounter);
+
+
+// let x = 3;
+// const y = x++;
+// console.log(x , y)
+
+// let a = 3;
+// const b = ++a;
+// console.log(a , b)
+
+
+// let x2 = 3n;
+// const y2 = x2++;
+// console.log(x2 , y2);
+
+
+let x2 = 3n;
+const y2 = ++x2;
+console.log(x2 , y2);
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);      //false
+// console.log(null == 0);     //false
+// console.log(null < 0);      //false
+// console.log(null >= 0);     //true
+// console.log(null <= 0);    //true
+
+
+// console.log(undefined == 0);  //false
+// console.log(undefined < 0);  //false
+// console.log(undefined > 0);  //false
+
+
+// === : strictly checks the value
+console.log("2" === 2);
