80aa4a89ebd69b1125654c690b27cb74896478a9
maaz962 <muhammadmaaz4405@gmail.com>
2025-07-09 04:16:48 +0000

    Newer Method Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,33 @@
+const name = "Maaz "
+const repoCount = 50
+//this concatination is an old method
+// console.log(name + repoCount + " Value");
+// This concatination is a newer method which is used nowadays. This is string interpolation
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+//Newer method for declaring variable in string. we use new keyword and object.
+//const gameName = new String('Maaz-programmer')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.indexOf('z'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(4,10)
+// console.log(anotherString);
+
+// const newStringOne = "     Maaz     "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://boredpanda.com/bored%20panda"
+// console.log(url.replace('%20', '-'))
+// console.log(url.includes("paisa"))
+
+const gameName = new String('maaz-arif-15')
+console.log(gameName.split('-'));
+
+
