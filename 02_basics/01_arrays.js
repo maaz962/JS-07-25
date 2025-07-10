a0fc64cf90500e2b5552b3641351bddb78fcf9ef
maaz962 <muhammadmaaz4405@gmail.com>
2025-07-10 17:59:30 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,21 @@
+// arrays
+// Shallow copy and deep copy
+// shallow copy: whose properties share the same references. yani jo b change kry ga original array me he change hoga
+// deep copy: whose properties do not share the same refernces.
+
+// const myArr = [0,1,2,3,4,5]
+// console.log(myArr[0]);
+
+// const myHeroes = ["Ronaldo", "Pepe"]
+// const myArr2 = new Array(1,2,3,4)
+
+// Array Methods
+const myArr = [0,1,2,3,4,5]
+// push values ko add krdeta hy, pop kisi b last value ko delete krdeta hy
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+myArr.unshift(9)
+console.log(myArr);
+
