14360a05862b35fc0d61a82cc322e2209efd75af
maaz962 <muhammadmaaz4405@gmail.com>
2025-07-11 06:29:13 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -10,12 +10,31 @@
 // const myArr2 = new Array(1,2,3,4)
 
 // Array Methods
-const myArr = [0,1,2,3,4,5]
+//const myArr = [0,1,2,3,4,5]
 // push values ko add krdeta hy, pop kisi b last value ko delete krdeta hy
 // myArr.push(6)
 // myArr.push(7)
 // myArr.pop()
 
-myArr.unshift(9)
-console.log(myArr);
+//myArr.unshift(9)
+// myArr.shift(9)
+//console.log(myArr);
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
 
+// const newArr = myArr.join()
+// console.log(newArr)
+// console.log(typeof newArr)
+
+//slice, splice
+const myArr = [0,1,2,3,4,5]
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("C", myArr);
+console.log(myn2);
