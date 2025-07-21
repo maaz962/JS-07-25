d2b3959ee67fcb5b51359e5de240e1c05a294577
maaz962 <muhammadmaaz4405@gmail.com>
2025-07-21 05:32:31 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,88 @@
+//hm ny dekha hmara pora code run ho rha hy, pr hamseha pora code run nai hona chahye. agr ap log-in ho to koi or code run hona chahye or agr log-out ho to koi or code run hona chahye,isko bola jata hy control flow ya logic flow.
+
+// Assignment Operator : =, 
+//comparison operator : <, >, <=, >=, ==, !=, === ,!==
+
+
+// const isUserLoggedIn = true
+// if(isUserLoggedIn){
+
+// }
+
+// if( 2 == "2"){
+//     console.log("executed")
+// }
+//ab ye dono 2 equal to nai hn pr phr b ye equal bta rha hy,aik num hy or dosra string hy, datatype ko b check krny ky liye hm use krty hn ===,
+// if( 2 === "2"){
+//     console.log("executed")
+// }
+
+//kuch b run nai huwa, keu k outside kuch likha he nai huwa.
+
+// if( 2 != 3){
+//     console.log("executed")
+// }
+
+// const isUserLoggedIn = true
+// const temperature = 41
+// if(temperature < 50){
+// console.log("less than 50");
+// }
+// console.log("temp is greater than 50");
+
+// const isUserLoggedIn = true
+// const temperature = 41
+// if(temperature === 41){
+// console.log("less than 50");
+// }
+// else{
+// console.log("temp is greater than 50");
+// }
+
+
+//var ka scope hy completely global, aik dfa scope sy bhr chla gya phr wo uska hissa nai hy , error dy ga, 
+// const score = 200
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+    
+// }
+// console.log(`User power: ${power}`);
+
+
+//Shorthand Notation.
+//idr implicit scope use kia hy, isme multiple me line nai likh sakty.
+// const balance = 1000
+// // if(balance > 500){
+
+// // }
+// //if(balance > 500) console.log("test");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } 
+// else if(balance < 750){
+// console.log("less than 750");
+// }
+// else if(balance < 900){
+// console.log("less than 900");
+// }
+// else {
+//     console.log("less than 1200");
+    
+// }
+
+// const userLoggedIn = true
+// const debitCard = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true
+
+// if(userLoggedIn && debitCard && 2==2){
+//     console.log("allow to buy courses");
+    
+// }
+// if(loggedInFromGoogle || loggedInFromEmail){
+//     console.log("User logged in");
+    
+// }
+
